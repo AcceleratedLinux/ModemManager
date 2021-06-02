3db015bf383db02dc74ac39b36040f1c6ef05093
Anubhav Gupta <anubhav.gupta@digi.com>
2021-06-02 16:37:39 -0400

    unitac: Changed dial_3gpp sequence
    Modified the dial_3gpp sequence for Unitac modems to not issue %APNCONF as well as
    to not detach from the network (+CGATT=0) before issuing attach (+CGATT=1). As per
    Unitac, detach is only needed if context changes and that is now handled by the host
    firmware.
    
    Signed-off-by: Anubhav Gupta <anubhav.gupta@digi.com>
    
    Unitac: Fixed wmode and bands
    
    1. Fixed WMODE? where modem manager was not parsing it correctly and causing the modem manager to make a connection on a wrong mode.
    2. Fixed supported bands, where it was missing BAND 72 and causing the current bands to also not show 72 in the list.
    
    Signed-off-by: Anubhav Gupta <anubhav.gupta@digi.com>
    
    Unitac: Expanded the plugin
    
    1. Removed MM_UNITAC_GET_SUPPORTED_BANDS_FROM_AT flag and moved to single method to get supported bands using AT!UBAND=?
    2. Changed the implementation to get RF information using AT!USTATUS instead of AT+CESQ
    3. Added DIAL_3GPP_STEP_PDP_ACTIVATE (AT+CGACT=1,cid) step in dial_3gpp
    
    Signed-off-by: Anubhav Gupta <anubhav.gupta@digi.com>
    
    1.Add "unlock retries". 2.Add "modem power down" 3.Get supported bands from AT!UBAND=?
    
    Signed-off-by: Anubhav Gupta <anubhav.gupta@digi.com>
    
    1.Deleted some code that was not called.
    
    Signed-off-by: Anubhav Gupta <anubhav.gupta@digi.com>
    
    Unitac: Updated Unitac plugin
    
    1. Seperated helper functions into a separate mm-modem-helpers-unitac.c file to align with rest of the plugins
    2. Changed the function to query the ICCID as ML620 uses proprietary command.
    3. Fixed dial_3gpp method as instructed by Unitac to make a proper connection.
    4. Fixed collection of IPv6 and IPv4 connection details.
    5. Changed udev port rules for ML620
    
    Signed-off-by: Anubhav Gupta <anubhav.gupta@digi.com>
    
    plugin: Added a new Unitac plugin
    
    Added a new Unitac plugin for ML660/ML620 LTE 450MHz modems.
    Unitac modem only supports AT commands
    
    Signed-off-by: Anubhav Gupta <anubhav.gupta@digi.com>
    
    meson: Add unitac plugin
    
    Added unitac plugin to meson build

---
+++ plugins/unitac/mm-broadband-modem-unitac.h
@@ -0,0 +1,51 @@
+/* -*- Mode: C; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details:
+ *
+ * Copyright (C) 2021 Anubhav Gupta <anubhav.gupta@digi.com>
+ */
+
+#ifndef MM_BROADBAND_MODEM_UNITAC_H
+#define MM_BROADBAND_MODEM_UNITAC_H
+
+#include "mm-broadband-modem.h"
+
+#define MM_TYPE_BROADBAND_MODEM_UNITAC            (mm_broadband_modem_unitac_get_type ())
+#define MM_BROADBAND_MODEM_UNITAC(obj)            (G_TYPE_CHECK_INSTANCE_CAST ((obj), MM_TYPE_BROADBAND_MODEM_UNITAC, MMBroadbandModemUnitac))
+#define MM_BROADBAND_MODEM_UNITAC_CLASS(klass)    (G_TYPE_CHECK_CLASS_CAST ((klass),  MM_TYPE_BROADBAND_MODEM_UNITAC, MMBroadbandModemUnitacClass))
+#define MM_IS_BROADBAND_MODEM_UNITAC(obj)         (G_TYPE_CHECK_INSTANCE_TYPE ((obj), MM_TYPE_BROADBAND_MODEM_UNITAC))
+#define MM_IS_BROADBAND_MODEM_UNITAC_CLASS(klass) (G_TYPE_CHECK_CLASS_TYPE ((klass),  MM_TYPE_BROADBAND_MODEM_UNITAC))
+#define MM_BROADBAND_MODEM_UNITAC_GET_CLASS(obj)  (G_TYPE_INSTANCE_GET_CLASS ((obj),  MM_TYPE_BROADBAND_MODEM_UNITAC, MMBroadbandModemUnitacClass))
+
+typedef struct _MMBroadbandModemUnitac MMBroadbandModemUnitac;
+typedef struct _MMBroadbandModemUnitacClass MMBroadbandModemUnitacClass;
+typedef struct _MMBroadbandModemUnitacPrivate MMBroadbandModemUnitacPrivate;
+
+
+
+struct _MMBroadbandModemUnitac {
+    MMBroadbandModem parent;
+    MMBroadbandModemUnitacPrivate *priv;
+};
+
+struct _MMBroadbandModemUnitacClass{
+    MMBroadbandModemClass parent;
+};
+
+GType mm_broadband_modem_unitac_get_type (void);
+
+MMBroadbandModemUnitac *mm_broadband_modem_unitac_new (const gchar *device,
+                                                       const gchar **drivers,
+                                                       const gchar *plugin,
+                                                       guint16 vendor_id,
+                                                       guint16 product_id);
+
+#endif /* MM_BROADBAND_MODEM_UNITAC_H */
